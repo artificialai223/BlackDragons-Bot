f3164197dfac56c6fe239a0a8811b3f4317ac4cd
Henry Walter <airstrikeminecraft@gmail.com>
2022-10-23 19:26:22 +0100

    Files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const sleekcmd = require('sleekcmd')
 
 sleekcmd.register({
-    token: "NzU3MjA0ODUwNTE2OTUxMTAx.GxlDFj.BTxLmP98o1b6C21lUBpP24UlvYxgM90NOqEVPo",
+    token: process.env.TOKEN,
     guild_id: "991342523454869564",
     intents: ["Guilds"],
     client_id: "757204850516951101"
